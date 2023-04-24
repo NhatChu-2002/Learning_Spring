cce4cbb0e1be38bcc4cd980de4dc36faeb3adf05
NhatChu <chunhat321123@gmail.com>
2023-04-24 09:04:12 +0700

    new commit

--- 03-speing-boot-hibernate-jpa-crud/01-cruddemo-student/src/main/java/com/luv2code/cruddemo/CruddemoApplication.java
+++ 03-speing-boot-hibernate-jpa-crud/01-cruddemo-student/src/main/java/com/luv2code/cruddemo/CruddemoApplication.java
@@ -17,8 +17,22 @@ public class CruddemoApplication {
 	public CommandLineRunner commandLineRunner(StudentDAO studentDAO)
 	{
 		return runner -> {
-			createStudent(studentDAO);
+			//createStudent(studentDAO);
+			createMultipleStudents(studentDAO);
 		};
+	}
+
+	private void createMultipleStudents(StudentDAO studentDAO) {
+		//create student object
+		System.out.println("Creating students ...");
+		Student tempStudent1 = new Student("John", "Doe","john@gmail");
+		Student tempStudent2 = new Student("Mary", "Apple","mary@gmail");
+		Student tempStudent3 = new Student("Kanji", "Hwang","kanji@gmail");
+		//save the student object
+		System.out.println("Save the student");
+		studentDAO.save(tempStudent1);
+		studentDAO.save(tempStudent2);
+		studentDAO.save(tempStudent3);
 	}
 
 	private void createStudent(StudentDAO studentDAO) {
