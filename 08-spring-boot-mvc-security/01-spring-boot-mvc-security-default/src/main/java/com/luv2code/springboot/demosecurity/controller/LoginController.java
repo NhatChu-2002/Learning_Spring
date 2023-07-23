0e2330dcde9dd1320295e779e26b385201ddfa48
NhatChu <chunhat321123@gmail.com>
2023-07-23 17:32:54 +0700

    finish section 8

--- 08-spring-boot-mvc-security/01-spring-boot-mvc-security-default/src/main/java/com/luv2code/springboot/demosecurity/controller/LoginController.java
+++ 08-spring-boot-mvc-security/01-spring-boot-mvc-security-default/src/main/java/com/luv2code/springboot/demosecurity/controller/LoginController.java
@@ -6,10 +6,19 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class LoginController {
     @GetMapping("/showMyLoginPage")
-    public String ShowMyLoginPage()
+    public String showMyLoginPage()
     {
 
         //return "plain-login";
         return "fancy-login";
+    }
+
+    // add request mapping for access denied
+    @GetMapping("/access-denied")
+    public String showAccessDenied()
+    {
+
+        //return "plain-login";
+        return "access-denied";
     }
 }
