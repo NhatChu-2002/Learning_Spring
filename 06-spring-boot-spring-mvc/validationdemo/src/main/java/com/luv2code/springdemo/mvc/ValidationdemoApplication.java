d9e54d1e743f672bde19881721fd3e8937e5f0ef
NhatChu <chunhat321123@gmail.com>
2023-08-01 19:55:01 +0700

    spring mvc form validation

---
+++ 06-spring-boot-spring-mvc/validationdemo/src/main/java/com/luv2code/springdemo/mvc/ValidationdemoApplication.java
@@ -0,0 +1,14 @@
+package com.luv2code.springdemo.mvc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+
+@SpringBootApplication
+public class ValidationdemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ValidationdemoApplication.class, args);
+	}
+
+}
