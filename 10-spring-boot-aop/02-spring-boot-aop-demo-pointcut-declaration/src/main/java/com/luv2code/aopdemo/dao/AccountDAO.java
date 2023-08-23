1572dc183e8b8bd73cbcbf6b8727a848f0a68650
NhatChu <chunhat321123@gmail.com>
2023-08-23 19:31:24 +0700

    @AfterReturning advice

--- 10-spring-boot-aop/02-spring-boot-aop-demo-pointcut-declaration/src/main/java/com/luv2code/aopdemo/dao/AccountDAO.java
+++ 10-spring-boot-aop/02-spring-boot-aop-demo-pointcut-declaration/src/main/java/com/luv2code/aopdemo/dao/AccountDAO.java
@@ -2,7 +2,10 @@ package com.luv2code.aopdemo.dao;
 
 import com.luv2code.aopdemo.Account;
 
+import java.util.List;
+
 public interface AccountDAO {
+    List<Account> findAccounts();
     void addAccount (Account theAccount, boolean vipFlag);
     boolean doWork();
     public String getName();
@@ -12,4 +15,6 @@ public interface AccountDAO {
     public String getServiceCode();
 
     public void setServiceCode(String serviceCode);
+
+
 }
